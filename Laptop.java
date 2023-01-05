c52325fc70adca2dd4ce3ba4fd058d38fd22d8ab
Gilang <gilangphb@gmail.com>
2023-01-06 02:28:38 +0700

    Menambahkan Laptop.java

---
+++ Laptop.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.latihan1;
+
+
+
+/**
+ *
+ * @author hp
+ */
+public class Laptop extends Komputer {
+    
+    private String merek;
+    
+   public void on(){
+        System.out.println("Laptop Menyala...");
+    }
+    
+     public void off(){
+        System.out.println("Laptop Mati...");
+    }
+}
