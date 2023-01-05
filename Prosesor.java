ce1c1bd8ee937b45fc21ba2c25318f9960cac406
Gilang <gilangphb@gmail.com>
2023-01-06 02:29:13 +0700

    Menambahkan Prosesor.java

---
+++ Prosesor.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.latihan1;
+
+/**
+ *
+ * @author hp
+ */
+public class Prosesor extends CPU {
+    
+    private String nama;
+    private int kecepatan;
+    
+    
+    @Override
+    void run(){
+        System.out.println("CPU Berjalan....");
+    }
+}
